a1eccb944f50a1da29311af5d7ead64807c1feb8
kerambit <liukang1488666228@gmail.com>
2021-05-17 21:21:29 +0400

    fix closing

--- src/scripts/components/Popup.js
+++ src/scripts/components/Popup.js
@@ -1,16 +1,15 @@
 export default class Popup {
   constructor(popupSelector) {
-    this._popupSelector = popupSelector;
-    this._popupSelector = document.querySelector(popupSelector);
+    this.element = document.querySelector(popupSelector);
   }
 
   open() {
-    this._popupSelector.classList.add("popup_opened");
+    this.element.classList.add("popup_opened");
     document.addEventListener("keydown", this._handleEscClose);
   }
 
   close = () => {
-    this._popupSelector.classList.remove("popup_opened");
+    this.element.classList.remove("popup_opened");
     document.removeEventListener("keydown", this._handleEscClose);
   }
 
@@ -21,9 +20,9 @@ export default class Popup {
   }
 
   setEventListeners() {
-    this._popupSelector.querySelector(".popup__close-button").addEventListener('click', () => {
-        this.close();
-  })
+    const closeBtn = this.element.querySelector(".popup__close-button");
+
+    closeBtn.addEventListener('click', this.close)
 }
 }
 
